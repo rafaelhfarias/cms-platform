5403816a86f858b0d23d447b1fdcbaa62e1d3a9c
Rafael Hipólito <rafael@alvin.ai>
2022-10-14 15:06:38 -0300

    first commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/rafaelhipolito/projects/cms-platform/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+   <div :class=" {'dark': darkMode}">
+        <h1 class="text-red-600 dark:text-dim-600">Hello from pages</h1>
+   </div>
+</template>
+
+<script setup>
+const darkMode = ref(true)
+
+</script>
